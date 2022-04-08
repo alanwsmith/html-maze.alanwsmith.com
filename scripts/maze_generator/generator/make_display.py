71bd3eb65efdfc992a53b649f6fc908fa7bee079
Alan Smith <commit@alanwsmith.com>
2022-04-08 01:36:59 -0400

    Set walls to be solid via background color

--- scripts/maze_generator/generator/make_display.py
+++ scripts/maze_generator/generator/make_display.py
@@ -2,7 +2,13 @@
 
 import json
 
+from string import Template
+
 json_data = None
+
+
+with open('template_display.html') as _tmpl:
+    chrome = Template(_tmpl.read())
 
 with open('maze.json') as _json:
     json_data = json.load(_json)
@@ -40,37 +46,40 @@ for row_index, row in enumerate(json_data['grid']):
             output[x_plus - 1][y_plus - 1] = 'x'
             output[x_plus + 1][y_plus - 1] = 'x'
 
+
 for row_index, row in enumerate(output):
     for cell_index, cell in enumerate(row):
         if cell == '.':
-            with open(f"output/{row_index}x{cell_index}.html", "w") as _out:
-                output_rows = '<table border="0" cellpadding="0">'
-                for r_index, r in enumerate(output):
-                    output_rows += '<tr>'
-                    for c_index, c in enumerate(r):
-                        x_plus = row_index + 1
-                        y_plus = cell_index + 1
-                        x_minus = row_index - 1
-                        y_minus = cell_index - 1
-                        if output[r_index][c_index] == 'x':
-                            output_rows += f"<td>&block;</td>"
+            output_rows = ''
+            for r_index, r in enumerate(output):
+                output_rows += '<tr>'
+                for c_index, c in enumerate(r):
+                    x_plus = row_index + 1
+                    y_plus = cell_index + 1
+                    x_minus = row_index - 1
+                    y_minus = cell_index - 1
+                    if output[r_index][c_index] == 'x':
+                        output_rows += f'<td width="26" height="26" bgcolor="#000000">&nbsp;</td>'
+                    else:
+                        if r_index == row_index and c_index == cell_index:
+                            output_rows += '<td align="center"><font color="##0fbf4d">&block;</font></td>'
+                        elif r_index == x_plus and c_index == cell_index and output[x_plus][cell_index] != 'x':
+                            output_rows += f'<td align="center"><a href="{x_plus}x{cell_index}.html"><font color="#449999">v</font></a></td>'
+                        elif r_index == x_minus and c_index == cell_index and output[x_minus][cell_index] != 'x':
+                            output_rows += f'<td align="center"><a href="{x_minus}x{c_index}.html"><font color="#449999">^</font></a></td>'
+                        elif r_index == row_index and c_index == y_plus and output[r_index][y_plus] != 'x':
+                            output_rows += f'<td align="center"><a href="{r_index}x{y_plus}.html"><font color="#449999">&gt;</font></a></td>'
+                        elif r_index == row_index and c_index == y_minus and output[r_index][y_minus] != 'x':
+                            output_rows += f'<td align="center"><a href="{r_index}x{y_minus}.html"><font color="#449999">&lt;</font></a></td>'
                         else:
-                            if r_index == row_index and c_index == cell_index:
-                                output_rows += '<td align="center">*</td>'
-                            elif r_index == x_plus and c_index == cell_index and output[x_plus][cell_index] != 'x':
-                                output_rows += f'<td align="center"><a href="{x_plus}x{cell_index}.html">?</a></td>'
-                            elif r_index == x_minus and c_index == cell_index and output[x_minus][cell_index] != 'x':
-                                output_rows += f'<td align="center"><a href="{x_minus}x{c_index}.html">?</a></td>'
-                            elif r_index == row_index and c_index == y_plus and output[r_index][y_plus] != 'x':
-                                output_rows += f'<td align="center"><a href="{r_index}x{y_plus}.html">?</a></td>'
-                            elif r_index == row_index and c_index == y_minus and output[r_index][y_minus] != 'x':
-                                output_rows += f'<td align="center"><a href="{r_index}x{y_minus}.html">?</a></td>'
-                            else:
-                                output_rows += f"<td></td>"
-                    output_rows += '</tr>'
-                output_rows += '</table>'
-                _out.write(output_rows)
+                            output_rows += f"<td></td>"
+                output_rows += '</tr>'
+
+            with open(f'../../../site/positions/{row_index}x{cell_index}.html', 'w') as _pos:
+                _pos.write(chrome.substitute(rows=output_rows))
+
         print(f"{cell}", end="")
     print()
+
 
 
